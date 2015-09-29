71252a1842487fcf1b17ec884c8ae2cf7451dd3f
DianaMDG <dianamdgoncalves@gmail.com>
2015-09-29 12:30:33 +0100

    nao guarda a matriz, so gera

--- tree.c
+++ tree.c
@@ -2,50 +2,34 @@
 #include<stdlib.h>
 #include<math.h>
 
-#define N 4
-#define POW 16
+#define N 7
 
-/*int seq[(int) pow(N,N-2)][N-2]; /*Array de ponteiros para onde vão ser guardadas as sequencias de Prüfer*/
-int seq[POW][N-2]; /*Array de ponteiros para onde vão ser guardadas as sequencias de Prüfer*/
-int linha_seq=0;
+
 
 /*Função recursiva que gera as sequências de Prüfer que vão gerar as árvores*/
 void gerar(int n, int casas, int gerado[]) {
 
     int i, j;
     if (casas > 1)
-        for( i = 0; i < n; i++) {
-            gerado[n-2-casas] = i+1; /*i+1 porque os valores são de 1 a n, e i vai de 0 a n-1 */
-            /*printf("%d %c", gerado[j], j< n-3-casas ? ',':']');*/
+        for( i = 0; i <= n; i++) {
+            gerado[n-2-casas] = i; /*i porque os valores são de 0 a n, e i vai de 0 a n */
             gerar (n, casas-1, gerado);
         }
     else
     /*Está na última casa da sequência*/
-        for ( i = 0; i<n ;i++) {
-            gerado[n-2-casas] = i+1; /*i+1 porque os valores são de 1 a n, e i vai de 0 a n-1*/
+        for ( i = 0; i<=n ;i++)
+            gerado[n-2-casas] = i; /*i+1 porque os valores são de 1 a n, e i vai de 0 a n-1*/
+            /*Imprime os valores das sequências*/
             /*printf("Nova sequência: [");
-            for ( j = 0; j < n-2; j++) {*/
-                printf("%d %s", gerado[j], j< n-3 ? ",\0":"]\n", seq);
-                /*seq[linha_seq][j] = gerado[j];*/
-            }
-            /*copiar gerado para *seq*/
-            linha_seq += 1;
-        }
+            for ( j = 0; j < n-2; j++) {
+                printf("%d %s", gerado[j], j< n-3 ? ",\0":"]\n");
+            }*/
 }
 
 int main() {
     
     int gerado[N-2];
-    int i, j;
     gerar(N, N-2, gerado);
     
-    /*imprimir a tabela das sequencias*/
-    printf("Sequências: [");
-    for (i = 0; i < POW; i++) {
-        for (j = 0; j < N-2; j++) {
-            printf("%d %s", seq[i][j], j< N-3 ? ",\0":"]\n[");
-            /*seq[i][j] = gerado[j];*/
-        }
-    }
     return 0;
 }
