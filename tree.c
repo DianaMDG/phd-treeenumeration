c80dc1dd089d27558eb519186180c88576757be7
DianaMDG <dianamdgoncalves@gmail.com>
2015-09-30 14:40:58 +0100

    refactor dos N pelos SIZE + N. Com erros no resto

--- tree.c
+++ tree.c
@@ -2,17 +2,11 @@
 #include<stdlib.h>
 #include<math.h>
 
-#define N 4
+#define N       4       /*N utilizado pela Marisol*/
+#define SIZE    N+1     /*Número de bits utilizado pela representação*/
+
 #define max(a,b) ((a) > (b) ? (a) : (b))
 #define min(a,b) ((a) < (b) ? (a) : (b))
-/*#define max(a,b) \
-   ({ __typeof__ (a) _a = (a); \
-       __typeof__ (b) _b = (b); \
-     _a > _b ? _a : _b; })
-#define min(a,b) \
-   ({ __typeof__ (a) _a = (a); \
-       __typeof__ (b) _b = (b); \
-     _a < _b ? _a : _b; })*/
 
 /*typedef uint16_t ele_t;*/
 typedef char ele_t;
@@ -24,29 +18,29 @@ struct node {
 };
 
 /*Matriz de adjacência triangular*/
-ele_t adjacencia[N*(N+1)/2];
-ele_t *linhas[N];
+ele_t adjacencia[(SIZE-1)*(SIZE)/2];
+ele_t *linhas[SIZE-1];
 
-ele_t adjacencia[N+1][N+1];
+/*ele_t adjacencia[SIZE][SIZE];*/
 
 /*Função que gera as árvores a partir das sequencias de Prüfer */
 void gerar_tree(int *seq){
     int i, j;
-    int degree[N+1];
+    int degree[SIZE];
 
     /*Primeiro passo da construção da árvore - construção array degree*/
-    for (i = 0; i <= N; i++){
+    for (i = 0; i < SIZE; i++){
         degree[i] = 1;
     }
-    for (i = 0; i < N-1; i++){
+    for (i = 0; i < SIZE-2; i++){
         /*printf("check2\n");*/
         degree[seq[i]] += 1;
     }
-    /*printf("%d\n", degree[N]);*/
+    /*printf("%d\n", degree[SIZE-1]);*/
 
     /*Definição das arestas do grafo e destruição do array degree*/
-    for (i=0; i < N-1 ; i++) { /* elementos da seq : 0 -> N-1 */
-        for (j = 0; j <= N; j++) { /*elementos do degree*/
+    for (i=0; i < SIZE-2 ; i++) { /* elementos da seq : 0 -> SIZE-2 */
+        for (j = 0; j < SIZE; j++) { /*elementos do degree*/
             if (degree[j] == 1) {
                 /*insert edge (seq[i], j) TODO*/
                 /*Caso adjacência*/
@@ -60,8 +54,8 @@ void gerar_tree(int *seq){
         }
     }
     /*Imprime a matriz de adjacencia*/
-    printf("matriz de adjacencia: %4d\n", N*(N+1)/2);
-    for (i=0;i< (N*(N+1)/2); i++){
+    printf("matriz de adjacencia: %4d\n", SIZE*(SIZE-1)/2);
+    for (i=0;i< (SIZE*(SIZE-1)/2); i++){
         printf("%d ", adjacencia[i]);
     }
     printf("\n");
@@ -72,19 +66,19 @@ void gerar_seq(int casas, int *gerado) {
 
     int i, j;
     if (casas > 1)
-        for( i = 0; i <= N; i++) {
-            gerado[N-1-casas] = i; /*i porque os valores são de 0 a n, e i vai de 0 a n */
+        for( i = 0; i < SIZE; i++) {
+            gerado[SIZE-2-casas] = i; /*i porque os valores são de 0 a n, e i vai de 0 a n */
             gerar_seq (casas-1, gerado);
         }
     else
     /*Está na última casa da sequência*/
-        for ( i = 0; i<=N ;i++){
-            gerado[N-1-casas] = i; /*i+1 porque os valores são de 1 a n, e i vai de 0 a n-1*/
+        for ( i = 0; i< SIZE ;i++){
+            gerado[SIZE-2-casas] = i; /*i+1 porque os valores são de 1 a n, e i vai de 0 a n-1*/
 
             /*Imprime os valores das sequências*/
             printf("Nova sequência: [");
-            for ( j = 0; j < N-1; j++)
-                printf("%d %s", gerado[j], j< N-2 ? ",\0":"]\n");
+            for ( j = 0; j < SIZE-2; j++)
+                printf("%d %s", gerado[j], j< SIZE-3 ? ",\0":"]\n");
             printf("Vai gerar nova árvore:\n");
             /*Gerar árvore associada à sequência*/
             gerar_tree(gerado);
@@ -93,20 +87,17 @@ void gerar_seq(int casas, int *gerado) {
 /*MAIN*/
 int main() {
 
-    int gerado[N-1];
+    int gerado[SIZE-2];
     int a=-1, i;
 
     /*Gerar matriz de adjacência e acessos a ela*/
-    for (i=0; i < N; i++) {
+    for (i=0; i < SIZE-1; i++) {
         linhas[i] = adjacencia + a;
         a+=N-i;
     }
-        /*Adjacência a zeros*/
-    /*for (i=0; i< (N*(N+1)/2); i++)
-        adjacencia[i]=0;*/
 
     /*Gerar sequências de Prüfer*/
-    gerar_seq(N-1, gerado);
+    gerar_seq(SIZE-2, gerado);
 
     return 0;
 }
