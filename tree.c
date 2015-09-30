34238ec8aa9e8c5914502bdcc03f378d85d80f67
DianaMDG <dianamdgoncalves@gmail.com>
2015-09-30 16:52:56 +0100

    correct SIZE*(SIZE-1); fun clear_adj; fun print_adj

--- tree.c
+++ tree.c
@@ -39,8 +39,8 @@ void generate_tree(int *seq){
     /*printf("%d\n", degree[SIZE-1]);*/
 
     /*Graph edge definition and degree array destruction*/
-    for (i = 0; i < SIZE-2 ; i++) { /* seq elements: 0 -> SIZE-2 */
-        for (j = 0; j < SIZE; j++) { /* degree elements*/
+    for (i = 0; i < (SIZE)-2 ; i++) { /* seq elements: 0 -> SIZE-2 */
+        for (j = 0; j < (SIZE); j++) { /* degree elements*/
             if (degree[j] == 1) {
                 /*insert edge (seq[i], j) TODO*/
                 /*Adjacency Case*/
@@ -54,16 +54,44 @@ void generate_tree(int *seq){
         }
     }
     /*print adjacency matrix*/
-    printf("Adjacency matrix: %4d\n", SIZE*(SIZE-1)/2);
-    for (i=0;i< (SIZE*(SIZE-1)/2); i++){
+    printf("Adjacency matrix: %4d\n", (SIZE)*(SIZE-1)/2);
+    for (i=0;i< ((SIZE)*(SIZE-1)/2); i++){
         printf("%d ", adjacency[i]);
     }
     printf("\n");
 }
 
 /*Function that prints the adjacency matrix of a given tree*/
-void print_adj(ele_t adjacency[]) {
+void print_adj(ele_t **lines) {
+    /*NOTE: Print adjacency matrix using array of pointers lines, and alligned to right.*/ 
+    /*WARNING: Size of stings is hard coded.*/
 
+    int i, j;
+    /*Print using adjacency array*/
+    /*printf("Print Adj matrix: %d\n",  (SIZE-1)*(SIZE)/2);
+    for (i=0; i < (SIZE-1)*(SIZE)/2; i++) {
+        printf("%d, ",adjacency[i]);
+    }
+    printf("\n");*/
+    
+    printf("Print adjacency matrix by lines:\n");
+    char temp[128];
+    for (i=0; i < (SIZE-1); i++) {
+        temp[0] = '\0';
+        for (j = i+1; j < (SIZE); j++) {
+            sprintf(temp, "%s%2d%c ", temp, lines[i][j], j<(SIZE-1) ? ',' : ' ');
+        }
+        printf("%16s\n", temp);
+    }
+}
+
+/*Function that clears an adjacency matrix -> sets to zeros */
+void clear_adj(ele_t adjacency[]) {
+    /*NOTE: */
+    int i;
+    for (i=0; i < (SIZE)*(SIZE-1)/2; i++) {
+        adjacency[i] = 0;
+    }
 }
 
 /*Recursive function that generates the Prüfer sequences that will generate the trees*/
@@ -80,8 +108,8 @@ void generate_seq(int spaces, int *generated) {
         }
     }
     else {
-        /*It is in the last sequence space*/
-        for (i = 0; i< SIZE ;i++){
+        /*It is now in the last sequence space*/
+        for (i = 0; i< (SIZE); i++) {
             generated[SIZE-2-spaces] = i;
 
             /*Prints the present sequence values*/
@@ -99,7 +127,7 @@ void generate_seq(int spaces, int *generated) {
 int main() {
 
     int generated[SIZE-2];
-    int i, j;
+    int i;
     int a = -1;
 
     /*Generate adjacency matrix and respective lines pointer array*/
@@ -108,39 +136,26 @@ int main() {
         a += N - i - 1; /*a += SIZE - 2 - i;*/
     }
     /*TEST: Set matrix values*/
-    for (i=0; i < (SIZE-1)*(SIZE)/2; i++) {
+    /*for (i=0; i < (SIZE-1)*(SIZE)/2; i++) {
         adjacency[i] = i+1;
-    }
-    /*Print adjacency matrix*/
-    printf("Print Adj matrix: %d\n",  (SIZE-1)*(SIZE)/2);
-    for (i=0; i < (SIZE-1)*(SIZE)/2; i++) {
-        printf("%d, ",adjacency[i]);
-    }
-    printf("\n");
+    }*/
+
     /*Print adjacency matrix by lines*/
-    printf("Print adjacency matrix by lines:\n");
-    char temp[128];
-    for (i=0; i < SIZE-1; i++) {
-        temp[0] = '\0';
-        for (j = i+1; j < SIZE; j++) {
-            /*printf("%d, ",linhas[i][j]);*/
-            sprintf(temp, "%s%2d, ", temp, lines[i][j]);
-        }
-        printf("%16s\n", temp);
-    }
+    print_adj(lines);
+    
     /*TEST: Change 2 lines of the matrix*/
-    j=2;
+    /*j=2;
     for (i=j+1; i < SIZE; i++) {
         lines[j][i] = 0;
-    }
+    }*/
     /*Print adjacency matrix by lines*/
-    printf("Print adjacency matrix by lines:\n");
+    /*printf("Print adjacency matrix by lines:\n");
     for (i=0; i < SIZE-1; i++) {
         for (j = i+1; j < SIZE; j++) {
             printf("%d, ",lines[i][j]);
         }
         printf("\n");
-    }
+    }*/
 
     /*Generate Prüfer sequences*/
     generate_seq(SIZE-2, generated);
