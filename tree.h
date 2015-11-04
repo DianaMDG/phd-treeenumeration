d5c7d7bc6870c71baec61f2bf10777ae6436e75c
DianaMDG <dianamdgoncalves@gmail.com>
2015-11-04 23:59:21 +0000

    adicionar defines - por codigo bonitinho

--- tree.h
+++ tree.h
@@ -2,11 +2,9 @@
 /*typedef uint16_t ele_t;*/
 typedef char ele_t;
 
-/*Indexed array -- NOT YET USED*/
-/*struct node {
-    int value;
-    struct node *children[N-1];
-};*/
+/*#define matriz      0*/
+
+
 
 /******************************************************************/
 /******************   FUNCTION Declaration   **********************/
@@ -19,31 +17,17 @@ void generate_seq(int spaces, int *generated);
 /*Function that generates trees from Prüfer sequences */
 void generate_tree(int *seq);
 
-/*Function that prints the adjacency list*/
-void print_list(void);
+#ifdef matriz
 
-/*Function that clears an adjacency list BUT THE (0,1) and (0,2) edges*/
-void clear_list(void);
-
-/*Function that generates the neutral network graph from the tree*/
-void generate_graph(void);
-
-void generate_graph_list(void);
-
-/*Function that verifies if the sequance is valid or not*/
-/*void check_seq(int *seq);*/
-
-/*Function that attributes zeros to the nodes, in a depth search*/
-void check_node(int node);
-
-/*Function used to delete repeated in adjacency list*/
-void verify_node(int node);
-
-/*Function used to generate combinations of connections to super node*/
-void recursive_list(int index);
+/*************************************************************************
+*                    Functions for the Adjacency Matrix
+*************************************************************************/
 
 /*Function used to generate combinations of connections to super node for building the adjacency matrix*/
 void recursive(int degree, int next_edge, int *edges);
+
+/*Function that generates the neutral network graph from the tree*/
+void generate_graph(void);
 
 /*Function that prints the adjacency matrix of a given tree*/
 void print_adj(void);
@@ -51,3 +35,27 @@ void print_adj(void);
 /*Function that clears an adjacency matrix */
 void clear_adj(void);
 
+#else
+/*************************************************************************
+*                    Functions for the Adjacency List
+*************************************************************************/
+
+/*Function used to delete repeated in adjacency list*/
+void verify_node(int node);
+
+/*Function used to generate combinations of connections to super node*/
+void recursive_list(int index);
+
+/*Function that generates the calculates the representation for a given tree*/
+void generate_graph_list(void);
+
+/*Function that attributes zeros to the nodes, in a depth search*/
+void check_node(int node);
+
+/*Function that prints the adjacency list*/
+void print_list(void);
+
+/*Function that clears an adjacency list BUT THE (0,1) and (0,2) edges*/
+void clear_list(void);
+
+#endif
