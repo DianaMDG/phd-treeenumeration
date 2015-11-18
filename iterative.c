a37916ab2d1d98ffbdb3cfd14f474266f3e5e596
DianaMDG <dianamdgoncalves@gmail.com>
2015-11-18 21:02:36 +0000

    add iterative files e inicio de logica dos defines

---
+++ iterative.c
@@ -0,0 +1,79 @@
+#include "tree.h"
+#include "iterative.h"
+
+void generate_seq_iteratively(int *generated) {
+    /*Iterative function that generates the Prüfer sequences that will generate the trees with the 1-0-2 super node*/
+    /* NOTE: may be outdated */
+
+    int i, j;
+    int div, res;
+    int base = SIZE - 2;
+    /*int index;*/
+
+    #if N == 15
+        for (i = 0; i < CORTE; i++) {
+    #else
+        for (i = 0; i < pow((SIZE-2), (SIZE-4)); i++) {
+    #endif
+        /*index = 0;*/
+        div = i / base;
+        res = i % base;
+        generated [SIZE-5] = res + 3;   /*3 comes from shifting the nodes not in the Super Node*/
+
+        for (j = SIZE-6 ; j > 1; j--) {
+        /*while (div != 0 || res != 0) {*/
+            res = div % base;
+            div = div / base;
+            generated [j] = res + 3; /*again, 3 ... */
+        }
+
+        /*just in case some missed*/
+        /*for (j = index; j < (SIZE - 4); j++) {
+            generated [j] = 3;*/ /*again, 3 ... */
+        /*}*/
+
+        #ifdef NUMBERS
+            SCount++;
+        #endif
+
+        /*int j; for ( j = 0; j < SIZE-4; j++) printf("%s%d %s",  j==0?"\nNew sequence: [":"", generated[j], j< SIZE-5 ? ",\0":"]\n");*/
+        #ifdef TOFILE
+            int j; for ( j = 0; j < SIZE-4; j++) fprintf(f_list, "%s%d %s",  j==0?"[":"", generated[j], j< SIZE-5 ? ",\0":"]\n");
+        #endif
+        /*Generate tree associated to the sequence generated*/
+        generate_tree(generated);
+    }
+}
+
+
+
+static void unfold_SN_list(void) {
+    /*Function used to generate combinations of connections to super node*/
+
+    int i = powb3[list_index[SIZE]], j;
+    int div, res;
+    int index; /*index do no ligado ao super no*/
+    /*int nodes[3] = {list_index[0], list_index[1], list_index[2]};*/
+
+    /*for (i = 0; i < powb3[list_index[SIZE]]; i++) {*/
+    while (i--) {
+        index = 0;                  /*restarting from the first node*/
+        for (j = 0, div = i; j < list_index[SIZE]; j++) {
+            res = div % 3;
+            div = div / 3;
+            list[res][list_index[res]++] = list[SIZE][index++];
+        }
+        #ifdef NUMBERS
+            CCount++;
+        #endif
+        #ifdef GRAPH
+            generate_graph_list();
+        #endif
+
+        /*reseting the list indexes TODO might be unecessary*/
+        list_index[0] = 2;
+        list_index[1] = 0;
+        list_index[2] = 0;
+    }
+}
+
--- tree.h
+++ tree.h
@@ -1,3 +1,42 @@
+
+/*List of defines*/
+/*#define NUMBERS     0        when defined : counts number of sequences, representations and cuts*/
+/*#define TOFILE      0        when defined : prints sequences, representations and cuts to files*/
+/*#define GRAPH       1        when defined : generates the representation associated with each sequence*/
+/*#define PRIM        0        when defined : checks if the tree is canonical using the Prim algorithm*/
+/*#define CONSTANTS   0        when defined : checks if the representation is minimal regarding constants*/
+/*#define ITERATIVE   0        when defined : generates the sequences and unfolds the super node iteratively. WARNING! Is slower!*/
+
+#ifdef NUMBERS
+    #pragma message "counting sequences, representations and cuts"
+#endif
+
+#ifdef TOFILE
+    #pragma message "printing to files"
+#endif
+
+#ifdef GRAPH
+    #pragma message "generating representations"
+#endif
+
+#ifdef PRIM
+    #ifndef GRAPH
+        #define GRAPH  1
+    #endif
+    #pragma message "calculating minimum spanning tree using Prim algorithm"
+#endif
+
+#ifdef CONSTANTS
+    #ifndef PRIM
+        #define PRIM  1
+    #endif
+    #ifndef GRAPH
+        #define GRAPH  1
+    #endif
+    #pragma message "Verifying if is minimal regarding constants"
+#endif
+
+
 /******************************************************************/
 /******************   FUNCTION Declaration   **********************/
 /******************************************************************/
@@ -5,9 +44,6 @@
 
 void generate_seq(int spaces, int *generated);
 /*Recursive function that generates the Prüfer sequences that will generate the trees*/
-
-void generate_seq_iteratively(int *generated);
-/*Iterative function that generates the Prüfer sequences that will generate the trees WITH THE SUPER NODE*/
 
 void generate_tree(int *seq);
 /*Function that generates trees from Prüfer sequences */
@@ -19,9 +55,6 @@ void generate_tree(int *seq);
 
 void unfold_SN_list_recursive(int index);
 /*Function used to generate combinations of connections to super node, recursively*/
-
-static void unfold_SN_list(void);
-/*Function used to generate combinations of connections to super node*/
 
 void generate_graph_list(void);
 /*Function that generates the representation for a given tree*/
